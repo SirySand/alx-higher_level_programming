cbc76e0af9f49355fedb11698bd7c4aec706b3d3
SirySand <georgessand11@gmail.com>
2023-02-23 16:38:26 -0800

    double digits

---
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+for i in range(100):
+    if i == 99:
+        print(f'{i}')
+    elif i < 10:
+        print('0{}, '.format(i), end='')
+    else:
+        print('{}, '.format(i), end='')
