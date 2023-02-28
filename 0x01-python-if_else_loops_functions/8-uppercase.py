495f00b792645470fc6bf5ca277544d66200e4b1
SirySand <georgessand11@gmail.com>
2023-02-28 03:08:49 -0800

    ord and chr

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    """
+    this is a function  that prints a given string in uppercase followed
+    by a new line.
+    """
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            print(chr(ord(i) - 32), end="")
+        elif i == str[-1]:
+            print(chr(ord(i) - 32))
+        else:
+            i += 1
