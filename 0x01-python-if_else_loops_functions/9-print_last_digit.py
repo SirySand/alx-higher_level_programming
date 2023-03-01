fef7e3db98d972ae9f4aabd0a0aaf665a1cc2d53
SirySand <georgessand11@gmail.com>
2023-03-01 08:55:16 -0800

    print_ladigit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -6,5 +6,5 @@ def print_last_digit(number):
     :return the last digit of number
     """
     last_digit = abs(number) % 10
-    print("{}".format(last_digit))
+    print("{}".format(last_digit), end='')
     return last_digit
