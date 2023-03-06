5faaa872851b5945e3be4cc8e6ce1daaaf22dd38
SirySand <georgessand11@gmail.com>
2023-03-05 18:37:01 -0800

    function add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -7,6 +7,5 @@ and use it here to do calculation
 "assigning values to variables"
 a = 1
 b = 2
-"calling the function to calculate"
-result = add(a, b)
-print("{} + {} = {}".format(a, b, result))
+
+print("{} + {} = {}".format(a, b, add(a, b)))
