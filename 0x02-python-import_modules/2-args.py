f64324348ef69347abd835dcdb3fac8d1b7056ff
SirySand <georgessand11@gmail.com>
2023-03-09 16:55:05 -0800

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == __main__:
+    import sys
+argv = sys.argv[1:]
+
+if len(argv) == 0:
+    print(len(argv), "argument.")
+elif len(argv) == 1:
+    print(len(argv), "argument.")
+    print("1:", argv[0])
+else:
+    print(len(argv), "arguments.")
+    for i in range(len(argv)):
+        print(str(i+1) + ":", arg[i])
