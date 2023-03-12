9f20f3f611e6e33dd69b53a6503e1cf3f280bd29
SirySand <georgessand11@gmail.com>
2023-03-12 16:44:12 -0700

    adding the infinite

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,12 +5,12 @@ if __name__ == "__main__":
 # chech that there are arguments
 if len(sys.argv) > 1:
     # Initialize the sum to 0
-    result_sum = 0
+    sum = 0
 
     # Iterate through the arguments
     for arg in sys.argv[1:]:
         # Cast each argument into integer
         arg = int(arg)
-        result_sum += arg
+        sum += arg
 # Print the result of the addition
-    print(result_sum)
+    print(sum)
