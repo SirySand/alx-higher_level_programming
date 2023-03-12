05f485b38249aeda4456476d085a81ff121eaea5
SirySand <georgessand11@gmail.com>
2023-03-12 16:39:38 -0700

    inf add

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -2,15 +2,15 @@
 if __name__ == "__main__":
     import sys
 
-#chech that there are arguments
+# chech that there are arguments
 if len(sys.argv) > 1:
-    #Initialize the sum to 0
+    # Initialize the sum to 0
     sum = 0
 
-    #Iterate through the arguments
+    # Iterate through the arguments
     for arg in sys.argv[1:]:
-        #Cast each argument into integer
+        # Cast each argument into integer
         arg = int(arg)
         result_sum += arg
-#Print the result of the addition
+# Print the result of the addition
     print(result_sum)
