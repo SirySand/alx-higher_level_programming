3749e6fcefc1a9a1f6987e73e04af5e7de69cbed
SirySand <georgessand11@gmail.com>
2023-03-15 15:10:06 -0700

    mysql databases

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-# Store MySQL username in a variable
-MYSQL_USER="root"
-
-# Store MySQL password in a variable
-MYSQL_PASSWORD="root"
-
-# Get list off databases
-databases='mysql -u $MYSQL_USER -p$MYSQL_PASSWORD -e"SHOW DATABASES;"|tr -d"|"|grep -v Database'
-
-# Display list of databases
-echo "List of databases:"
-echo $databases
+mysql -uroot -p -e "SHOW DATABASES;"
