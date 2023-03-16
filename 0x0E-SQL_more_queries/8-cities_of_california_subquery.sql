d4f64206c29c0c25736d778f35105f5b33d0ea79
SirySand <georgessand11@gmail.com>
2023-03-16 08:08:29 -0700

    city of california

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,29 +1,5 @@
 -- a script that list all the cities of a state found in a database
 -- JOIN keyword is not allowed
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-	id INT AUTO_INCREMENT PRIMARY KEY UNIQUE NOT NULL,
-	name VARCHAR(256) NOT NULL
-);
-INSERT IGNORE INTO states (
-	name
-)VALUES(
-'California'
-);
-INSERT IGNORE INTO cities (
-	name
-)VALUES(
-'San Francisco'
-);
-INSERT IGNORE INTO cities (
-        name
-)VALUES(
-'San Jose'
-);
-INSERT IGNORE INTO cities (
-        name
-)VALUES(
-'Los Angeles'
-);
 SELECT cities.id, cities.name
 FROM cities
 WHERE state_id = (SELECT id FROM states WHERE name = "California")
