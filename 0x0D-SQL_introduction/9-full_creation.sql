c3ecdb8945824168e9d347eaf8c2aa93d3e94412
SirySand <georgessand11@gmail.com>
2023-03-16 20:08:52 -0700

    full create

---
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -0,0 +1,43 @@
+-- a script 
+-- SELECT and SHOW not allowed
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+INSERT IGNORE INTO second_table (
+	id,
+	name,
+	score
+) VALUES (
+	1,
+	'JOHN',
+	10
+);
+INSERT IGNORE INTO second_table (
+	id,
+	name,
+	score
+) VALUES (
+	2,
+	'Alex',
+	3
+);
+INSERT IGNORE INTO second_table (
+	id,
+	name,
+	score
+) VALUES (
+	3,
+	'Bob',
+	14
+);
+INSERT IGNORE INTO second_table (
+	id,
+	name,
+	score
+) VALUES (
+	4,
+	'George',
+	8
+);
