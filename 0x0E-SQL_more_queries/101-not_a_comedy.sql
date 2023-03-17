ca7baf286a70a8d35e9f8c0491e4ce6bf36bf520
SirySand <georgessand11@gmail.com>
2023-03-16 18:08:18 -0700

    not a comedy

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,7 @@
+-- a script that lists all comedy shows in hbtn_0d_tvshows
+-- ascending order by show title, use only one SELECT statement
+SELECT tv_shows.title FROM tv_shows
+INNER JOIN tv_genres
+ON tv_genres.id = tv_shows.id
+WHERE tv_genres.name != "Comedy"
+ORDER BY tv_shows.title ASC;
