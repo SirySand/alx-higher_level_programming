87e7d3cbbfe65cdcb0ca7415167c3c00f33eccf6
SirySand <georgessand11@gmail.com>
2023-03-16 19:33:52 -0700

    table first

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- a script that creates a table in the current database
 -- should not fail if the table exists, SELECT or SHOW not allowed
-CREATE TABLE first_table IF NOT EXISTS (
+CREATE TABLE IF NOT EXISTS first_table (
 	id INT,
 	name VARCHAR(256)
 );
