b7628578873bc48076fe09a7be7271b4efb908fe
SirySand <georgessand11@gmail.com>
2023-04-05 02:46:42 -0700

    homeworks2

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-"""This is a function that prints all integers 
-   of a list.
+"""This is a function that prints all integers of a list.
 """
-def print_list_integer(my_list=[]):   
+
+
+def print_list_integer(my_list=[]):
     for i in range(my_list):
-        print("", str.format(i))
+        print("", format(i))
