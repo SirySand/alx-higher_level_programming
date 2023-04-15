90e3e9f3780ebb40d077dff0dd4094d874839864
SirySand <georgessand11@gmail.com>
2023-04-15 19:18:47 +0000

    multi multi language

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -3,7 +3,7 @@
 */
 const myC = 'C is fun';
 const myPython = 'Python is cool';
-const myJavascript = 'Javascript is amazing';
+const myJavascript = 'JavaScript is amazing';
 console.log(myC);
 console.log(myPython);
 console.log(myJavascript);
