880fb9792def2e7fbad91b0390e3419d5ec299a5
SirySand <georgessand11@gmail.com>
2023-04-16 17:02:40 +0000

    argv process

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+import {argv} from 'node:process';
+
+if argv[$index] < 2 => {
+  console.log('No argument');
+};
+else if argv[$index] = 2 => {
+  console.log('Argument found');
+};
+else argv[$index] > 2 => {
+  console.log['Arguments found');
+}; 
