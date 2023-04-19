f157c9fbcddf6b46da2922d47a9d7f10a000d0bd
SirySand <georgessand11@gmail.com>
2023-04-19 23:51:55 +0000

    if and argv

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
 
-import {argv} from 'node:process';
-
 const numArg = process.argv.length - 2;
 
 if (numArg < 2){
   console.log('No argument');
-}
-else if (numArg === 2){
+} else if (numArg === 2) {
   console.log('Argument found');
-}else{
+} else {
   console.log['Arguments found');
 }
