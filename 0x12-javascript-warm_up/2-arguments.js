39f2c3da398f9fe68cc28a2bc7d6b1c42733b96b
SirySand <georgessand11@gmail.com>
2023-04-20 00:00:12 +0000

    arguments pro

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,10 +2,10 @@
 
 const numArg = process.argv.length - 2;
 
-if (numArg < 2){
+if (numArg < 2) {
   console.log('No argument');
 } else if (numArg === 2) {
   console.log('Argument found');
 } else {
-  console.log['Arguments found');
+  console.log('Arguments found');
 }
